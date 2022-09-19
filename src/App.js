35badc95af57d323381cbf78fdff0ac3a63fc637
HUNGNAM\truon <truongnhuhungnam@gmail.com>
2022-09-19 21:11:20 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { useStore } from "./store";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, dispatch] = useStore();
+
+    return (
+        <div>
+            <input type="text" value={state.todoInput} />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/store/reducer.js
@@ -0,0 +1,14 @@
+const initState = {
+    todos: [],
+    todoInput: "",
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        default:
+            throw new Error("Invalid action type");
+    }
+};
+
+export { initState };
+export default reducer;
